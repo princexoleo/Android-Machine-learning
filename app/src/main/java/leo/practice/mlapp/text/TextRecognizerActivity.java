313f713f2fdbe9b9321192b33aad2aa7d882210b
maz_leo <xtleon.xl@gmail.com>
2019-09-25 01:43:35 +0600

    text recognizer button initialize

--- app/src/main/java/leo/practice/mlapp/text/TextRecognizerActivity.java
+++ app/src/main/java/leo/practice/mlapp/text/TextRecognizerActivity.java
@@ -3,14 +3,29 @@ package leo.practice.mlapp.text;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import leo.practice.mlapp.R;
 
 public class TextRecognizerActivity extends AppCompatActivity {
+    private static final String TAG = "TextRecognizerActivity";
+
+    private Button captureButton, detectButton;
+    private ImageView cameraImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_text_recognizer);
+
+        // initialize all the components
+        initialize();
+    }
+
+    private void initialize() {
+        captureButton = findViewById(R.id.capture_button_id);
+        detectButton = findViewById(R.id.detect_button_id);
+        cameraImageView = findViewById(R.id.imageView);
     }
 }
