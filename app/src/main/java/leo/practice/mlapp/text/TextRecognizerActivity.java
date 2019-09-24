6901426a389bba25270d4774e39a76fb5bd9192f
maz_leo <xtleon.xl@gmail.com>
2019-09-25 02:18:59 +0600

    camera open workind success

--- app/src/main/java/leo/practice/mlapp/text/TextRecognizerActivity.java
+++ app/src/main/java/leo/practice/mlapp/text/TextRecognizerActivity.java
@@ -2,7 +2,11 @@ package leo.practice.mlapp.text;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
@@ -10,9 +14,10 @@ import leo.practice.mlapp.R;
 
 public class TextRecognizerActivity extends AppCompatActivity {
     private static final String TAG = "TextRecognizerActivity";
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     private Button captureButton, detectButton;
-    private ImageView cameraImageView;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +26,48 @@ public class TextRecognizerActivity extends AppCompatActivity {
 
         // initialize all the components
         initialize();
+
+        captureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
+
+        detectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = null;
+            if (extras != null) {
+                imageBitmap = (Bitmap) extras.get("data");
+            }
+            imageView.setImageBitmap(imageBitmap);
+        }
+    }
+
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
 
     private void initialize() {
         captureButton = findViewById(R.id.capture_button_id);
         detectButton = findViewById(R.id.detect_button_id);
-        cameraImageView = findViewById(R.id.imageView);
+        imageView = findViewById(R.id.imageView);
     }
 }
